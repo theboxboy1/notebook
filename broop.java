0d9ed92db207f18983df36cae93b7e7949910473
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-09-13 13:02:17 -0400

    Add files via upload

---
+++ broop.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.print("T");
+    System.out.print("R");
+    System.out.print("I");
+    System.out.print("A");
+    System.out.print("N");
+    System.out.print("G");
+    System.out.print("L");
+    System.out.print("E");
+    System.out.println("");
+    System.out.println("SQUARE");
+    System.out.println("Q    R");
+    System.out.println("U    A");
+    System.out.println("A    U");
+    System.out.println("R    Q");
+    System.out.println("ERAUQS");
+    
+  }
+}
