229784450baffdaa72cf89422925cb20679c07a5
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-09-21 13:33:32 -0400

    Create average.java

---
+++ average.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+  
+    int a,b; 
+    a = 7; 
+    b = 3;
+    double average = (a + b)/2;
+    
+    System.out.println("Average = " + average);
+
+
+
+  
+  }
+}
