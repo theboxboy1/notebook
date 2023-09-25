cdfb2077eb855b07cf138e0f6f09902ddeb9c41c
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-09-25 14:01:52 -0400

    Create if statement.java

---
+++ if statement.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+  
+  int i=2; int j=3;
+    if (i>j)
+    {
+      System.out.println("i is greater");
+      
+    }
+    if (i==j)
+
+    {
+
+      System.out.println(" i is greater than 3");
+    }
+
+
+
+
+  }
+}
