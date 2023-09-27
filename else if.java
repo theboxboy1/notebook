d03266a1179c4dc089b223fe556e87634834a568
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-09-27 13:34:48 -0400

    Create else if.java

---
+++ else if.java
@@ -0,0 +1,26 @@
+class Main { 
+  public static void main(String[] args) {
+  
+  int s = 25;
+  if (s > 30)
+    {// true statement
+      System.out.println("Number greater than 30");
+      
+    }
+    else if (s>20)
+      {  
+      // else block
+      System.out.println("Number greater than 20");
+
+      }
+      else if (s>10)
+      {
+        System.out.println("Number greater than 10");
+      }
+
+
+
+
+  }
+}
+              
