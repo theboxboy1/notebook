6f9a42424d85964a46d857ca72010f708f46bfe2
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-09-29 13:36:01 -0400

    Create relational_operators.java

---
+++ relational_operators.java
@@ -0,0 +1,8 @@
+int num1 = 10;
+int num2 = 5;
+a = (num1 == num2);     // false
+b = (num1 != num2);  // true
+c = (num1 > num2);    // true
+d = (num1 < num2);       // false
+e = (num1 >= num2); // true
+f = (num1 <= num2);    // false
