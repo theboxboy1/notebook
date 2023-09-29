08ae65e11c3feab866547faaefc6f06b75eacdef
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-09-29 13:33:51 -0400

    Create arithmetic operators.java

---
+++ arithmetic operators.java
@@ -0,0 +1,12 @@
+int a = 10;
+int b = 5;
+int sum = a + b;
+int difference = a - b;
+int product = a * b;
+int quotient = a / b;
+int remainder = a % b;
+a++; // Increment 'a' by 1
+b--; // Decrement 'b' by 1
+
+
+
