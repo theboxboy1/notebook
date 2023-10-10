0836f45fc688c6d8868849a359e34aa816fe25c1
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-10-10 13:11:38 -0400

    Create Program2.java

---
+++ Program2.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+
+
+class Main { 
+  public static void main(String[] args) {
+    
+   System.out.println("oOOO()");
+   System.out.println(" /  _)");
+   System.out.println(" |  (");
+   System.out.println("  \\_)  ()OOOo");
+
+   
+   System.out.println("       (_  \\ ");
+   System.out.println("        )  | ");
+   System.out.println("       (_ /  ");
+
+
+
+   System.out.println("oOOO()");
+   System.out.println(" /  _)");
+   System.out.println(" |  (");
+   System.out.println("  \\_)  ()OOOo");
+
+   
+   System.out.println("       (_  \\ ");
+   System.out.println("        )  | ");
+   System.out.println("       (_ /  ");
+
+
+
+   
+   
+    
+}
+
+}
+
