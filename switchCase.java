ee364e0f94d55a9a2f8589b67b8fff8fcbfeb772
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-10-16 13:53:04 -0400

    Create switchCase.java

---
+++ switchCase.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner; // imports scanner
+
+public class App {
+    public static void main(String[] args) {
+       
+
+        char x = 'f';
+
+      switch(x){
+      
+      case 's':
+        System.out.println("90");
+        break;
+
+        case 'a':
+        System.out.println("80");
+        break;
+
+      default:
+        System.out.println("cases other than s and a were not found");
+
+      }
+
+    }
+    
+    
+    
+    }
+
+
+
+
