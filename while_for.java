89e811657fc842e75f9085784d71fc5b67a2fd0a
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-10-18 13:42:14 -0400

    Create while_for.java

---
+++ while_for.java
@@ -0,0 +1,28 @@
+
+public class App {
+    public static void main(String[] args) {
+        
+        int i = 15;
+        while(i >= 10){
+            System.out.println(i);
+            i--;
+        }
+
+       
+    /*
+    for(int j=0;j<5;j++){
+        System.out.println(j);
+    }
+    */
+    
+    }
+    
+
+
+}
+    
+
+
+
+
+
