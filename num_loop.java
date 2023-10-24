e877f63827471d32aa0ca9867c725bf89b01099d
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-10-24 13:43:47 -0400

    Create num_loop.java

---
+++ num_loop.java
@@ -0,0 +1,35 @@
+
+public class App {
+    public static void main(String[] args) {
+        
+        
+     int i = 15;
+     
+     while(i<=30){
+        System.out.println(i);
+        i++;
+        
+     }
+    
+
+
+    
+   
+
+}
+
+
+
+
+
+
+    
+
+
+}
+    
+
+
+
+
+
