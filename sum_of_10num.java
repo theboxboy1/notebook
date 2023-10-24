7f53d7c81e523d91b2d1219bcfa0410fc7975f2f
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-10-24 13:33:28 -0400

    Create sum_of_10num.java

---
+++ sum_of_10num.java
@@ -0,0 +1,36 @@
+
+public class App {
+    public static void main(String[] args) {
+        
+        
+        
+        int Sum = 0;
+
+        for (int i = 0; i <= 10; i = i+2 ){
+            Sum = Sum + i;
+
+        }
+        System.out.println(Sum);
+
+
+
+    
+   
+
+}
+
+
+
+
+
+
+    
+
+
+}
+    
+
+
+
+
+
