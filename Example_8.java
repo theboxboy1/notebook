803e83abdf5e81a648e9bffe33bfbf27284e12ae
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-10-25 23:59:42 -0400

    Create Example_8.java

---
+++ Example_8.java
@@ -0,0 +1,35 @@
+
+
+public class App {
+    public static void main(String[] args) {
+        
+        
+        
+     for(int i = 1; i<=1000; i++){
+        
+        if (i ==300){
+            break;
+        }
+        System.out.println(i);
+     }
+
+    
+   
+
+}
+
+
+
+
+
+
+    
+
+
+}
+    
+
+
+
+
+
