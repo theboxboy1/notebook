6f617e34392fd423129fb845a5b4e18735f8b15e
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-10-25 23:34:06 -0400

    Create Example_1.java

---
+++ Example_1.java
@@ -0,0 +1,36 @@
+
+public class App {
+    public static void main(String[] args) {
+        
+        
+    int i = 1;
+
+    while(i<=5){
+        System.out.println(i);
+        i++;
+    }
+
+    
+    
+
+
+    
+   
+
+}
+
+
+
+
+
+
+    
+
+
+}
+    
+
+
+
+
+
