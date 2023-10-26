07e1d420313759419f164084f59b5951343283fb
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-10-25 23:49:34 -0400

    Create Example_5.java

---
+++ Example_5.java
@@ -0,0 +1,37 @@
+
+
+public class App {
+    public static void main(String[] args) {
+        
+        
+     int counter = 1;
+     String text = "text";
+
+     do{
+        System.out.println(text);
+        counter++;
+     }
+    
+     while(counter<= 5);
+
+
+    
+   
+
+}
+
+
+
+
+
+
+    
+
+
+}
+    
+
+
+
+
+
