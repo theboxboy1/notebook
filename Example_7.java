cfa63353718b58ca0f4dbdd44a43d5bc52d97818
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-10-25 23:54:23 -0400

    Create Example_7.java

---
+++ Example_7.java
@@ -0,0 +1,33 @@
+
+
+public class App {
+    public static void main(String[] args) {
+        
+        int sum = 0;
+        int n = 1000;
+        
+     for(int i = 1; i<=n; i++){
+        sum = sum + i;
+     }
+
+System.out.println("Sum: " + sum);
+    
+   
+
+}
+
+
+
+
+
+
+    
+
+
+}
+    
+
+
+
+
+
