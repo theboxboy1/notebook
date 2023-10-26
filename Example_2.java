22f6b8496a24b764348b3bd53cc3ffdcc7d1d253
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-10-25 23:36:21 -0400

    Create Example_2.java

---
+++ Example_2.java
@@ -0,0 +1,39 @@
+
+public class App {
+    public static void main(String[] args) {
+        
+        
+    int i = 1;
+    int sum = 0;
+
+    while(i<=5){
+        System.out.println(i);
+        i++;
+        sum = sum + i;
+    }
+
+    System.out.println("The sum of the above numbers is: " + sum);
+    
+    
+
+
+    
+   
+
+}
+
+
+
+
+
+
+    
+
+
+}
+    
+
+
+
+
+
